58312c1c5fab4389d1dfc4d7e16de331159f3755
sioncamara <sioncamara@gmail.com>
2024-03-17 10:52:30 -0500

    feat: enhance like button when clicking
    ring around button turns darker on click for both light and dark mode

--- app/likes.tsx
+++ app/likes.tsx
@@ -48,28 +48,35 @@ export default function Likes({
     router.refresh()
   }
   return (
-    <button onClick={handleLikes} className="group flex items-center">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        viewBox="0 0 24 24"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className={` ring-offset-1 group-hover:rounded-full group-hover:bg-rose-50 group-hover:ring-8 group-hover:ring-rose-50 group-active:bg-rose-100  ${
-          tweet.user_has_liked_tweet
-            ? "fill-rose-600 stroke-rose-600"
-            : "fill-none stroke-gray-500 group-hover:stroke-rose-600 "
-        }`}
-      >
-        <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-      </svg>
+    <button onClick={handleLikes} className="group mt-3 flex items-center">
       <span
-        className={`ml-1 text-sm  ${
+        className="flex items-center justify-center rounded-full group-hover:bg-pink-50 group-hover:ring-8 group-hover:ring-pink-50 group-active:bg-pink-100 group-active:ring-pink-100 
+      dark:group-hover:bg-[#270c14] dark:group-hover:ring-[#270c14] dark:group-active:bg-pink-950 dark:group-active:ring-pink-950
+      "
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className={`   ${
+            tweet.user_has_liked_tweet
+              ? "fill-pink-600 stroke-pink-600"
+              : "fill-none stroke-gray-500 group-hover:stroke-pink-600 "
+          }`}
+        >
+          <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+        </svg>
+      </span>
+
+      <span
+        className={` ml-1 text-sm ${
           tweet.user_has_liked_tweet
-            ? "text-rose-600"
-            : "text-gray-500 group-hover:text-rose-600"
+            ? "text-pink-600"
+            : "text-gray-500 group-hover:text-pink-600"
         }`}
       >
         {tweet.likes}
